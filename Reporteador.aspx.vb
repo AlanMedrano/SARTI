c887001145069859e082b14686716b5bc1ecfdcf
AlanMedrano <aemedranom@gmail.com>
2020-01-20 22:55:16 -0600

    se agrega validacion de logeo

--- Reporteador.aspx.vb
+++ Reporteador.aspx.vb
@@ -2,6 +2,10 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Session("Correo") = "" Then ' Valido usuario logeado.
+            Response.Redirect("Default.aspx")
+        End If
+
         Dim consulta As Integer
         If (IsPostBack) Then
             consulta = Page.Request.Form("ddl_consulta")
